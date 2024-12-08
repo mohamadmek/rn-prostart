83d84661f2846e010fdc29841e6b089c4dad7875
Mohamad Meksasi <45299765+mohamadmek@users.noreply.github.com>
2024-12-08 16:31:36 +0100

    set up github actions

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,18 @@
 #! /usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { Command } = require("commander");
-const figlet = require("figlet");
-const fs = require("fs");
-const path = require("path");
+const { Command } = require('commander');
+const figlet = require('figlet');
+const fs = require('fs');
+const path = require('path');
 const program = new Command();
-console.log(figlet.textSync("ProStart"));
+console.log(figlet.textSync('ProStart'));
 program
-    .version("0.0.1")
-    .description("An example CLI for managing a directory")
-    .option("-l, --ls  [value]", "List directory contents")
-    .option("-m, --mkdir <value>", "Create a directory")
-    .option("-t, --touch <value>", "Create a file")
+    .version('0.0.1')
+    .description('An example CLI for managing a directory')
+    .option('-l, --ls  [value]', 'List directory contents')
+    .option('-m, --mkdir <value>', 'Create a directory')
+    .option('-t, --touch <value>', 'Create a file')
     .parse(process.argv);
 const options = program.opts();
 async function listDirContents(filepath) {
@@ -21,28 +21,28 @@ async function listDirContents(filepath) {
         const detailedFilesPromises = files.map(async (file) => {
             let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
             const { size, birthtime } = fileDetails;
-            return { filename: file, "size(KB)": size, created_at: birthtime };
+            return { filename: file, 'size(KB)': size, created_at: birthtime };
         });
         // add the following
         const detailedFiles = await Promise.all(detailedFilesPromises);
         console.table(detailedFiles);
     }
     catch (error) {
-        console.error("Error occurred while reading the directory!", error);
+        console.error('Error occurred while reading the directory!', error);
     }
 }
 function createDir(filepath) {
     if (!fs.existsSync(filepath)) {
         fs.mkdirSync(filepath);
-        console.log("The directory has been created successfully");
+        console.log('The directory has been created successfully');
     }
 }
 function createFile(filepath) {
-    fs.openSync(filepath, "w");
-    console.log("An empty file has been created");
+    fs.openSync(filepath, 'w');
+    console.log('An empty file has been created');
 }
 if (options.ls) {
-    const filepath = typeof options.ls === "string" ? options.ls : __dirname;
+    const filepath = typeof options.ls === 'string' ? options.ls : __dirname;
     listDirContents(filepath);
 }
 if (options.mkdir) {
