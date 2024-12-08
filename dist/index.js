740990f8b2de9185a5ee260fef7df8df4064e2f5
Mohamad Meksasi <45299765+mohamadmek@users.noreply.github.com>
2024-12-08 16:29:51 +0100

    set up github actions

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,6 @@
 #! /usr/bin/env node
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+Object.defineProperty(exports, "__esModule", { value: true });
 const { Command } = require("commander");
 const figlet = require("figlet");
 const fs = require("fs");
@@ -23,23 +15,21 @@ program
     .option("-t, --touch <value>", "Create a file")
     .parse(process.argv);
 const options = program.opts();
-function listDirContents(filepath) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const files = yield fs.promises.readdir(filepath);
-            const detailedFilesPromises = files.map((file) => __awaiter(this, void 0, void 0, function* () {
-                let fileDetails = yield fs.promises.lstat(path.resolve(filepath, file));
-                const { size, birthtime } = fileDetails;
-                return { filename: file, "size(KB)": size, created_at: birthtime };
-            }));
-            // add the following
-            const detailedFiles = yield Promise.all(detailedFilesPromises);
-            console.table(detailedFiles);
-        }
-        catch (error) {
-            console.error("Error occurred while reading the directory!", error);
-        }
-    });
+async function listDirContents(filepath) {
+    try {
+        const files = await fs.promises.readdir(filepath);
+        const detailedFilesPromises = files.map(async (file) => {
+            let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
+            const { size, birthtime } = fileDetails;
+            return { filename: file, "size(KB)": size, created_at: birthtime };
+        });
+        // add the following
+        const detailedFiles = await Promise.all(detailedFilesPromises);
+        console.table(detailedFiles);
+    }
+    catch (error) {
+        console.error("Error occurred while reading the directory!", error);
+    }
 }
 function createDir(filepath) {
     if (!fs.existsSync(filepath)) {
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 #! /usr/bin/env node
-const { Command } = require("commander");
-const figlet = require("figlet");
-const fs = require("fs");
-const path = require("path");
+const { Command } = require('commander');
+const figlet = require('figlet');
+const fs = require('fs');
+const path = require('path');
 
 const program = new Command();
 
-console.log(figlet.textSync("ProStart"));
+console.log(figlet.textSync('ProStart'));
 
 program
-  .version("0.0.1")
-  .description("An example CLI for managing a directory")
-  .option("-l, --ls  [value]", "List directory contents")
-  .option("-m, --mkdir <value>", "Create a directory")
-  .option("-t, --touch <value>", "Create a file")
+  .version('0.0.1')
+  .description('An example CLI for managing a directory')
+  .option('-l, --ls  [value]', 'List directory contents')
+  .option('-m, --mkdir <value>', 'Create a directory')
+  .option('-t, --touch <value>', 'Create a file')
   .parse(process.argv);
 
 const options = program.opts();
@@ -24,30 +24,30 @@ async function listDirContents(filepath: string) {
     const detailedFilesPromises = files.map(async (file: string) => {
       let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
       const { size, birthtime } = fileDetails;
-      return { filename: file, "size(KB)": size, created_at: birthtime };
+      return { filename: file, 'size(KB)': size, created_at: birthtime };
     });
     // add the following
     const detailedFiles = await Promise.all(detailedFilesPromises);
     console.table(detailedFiles);
   } catch (error) {
-    console.error("Error occurred while reading the directory!", error);
+    console.error('Error occurred while reading the directory!', error);
   }
 }
 
 function createDir(filepath: string) {
   if (!fs.existsSync(filepath)) {
     fs.mkdirSync(filepath);
-    console.log("The directory has been created successfully");
+    console.log('The directory has been created successfully');
   }
 }
 
 function createFile(filepath: string) {
-  fs.openSync(filepath, "w");
-  console.log("An empty file has been created");
+  fs.openSync(filepath, 'w');
+  console.log('An empty file has been created');
 }
 
 if (options.ls) {
-  const filepath = typeof options.ls === "string" ? options.ls : __dirname;
+  const filepath = typeof options.ls === 'string' ? options.ls : __dirname;
   listDirContents(filepath);
 }
 
